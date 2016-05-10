be6f1f5efe6f0c233c133dc273ea480cb7ad2b3b
Leandro Costa <leandrocgsi@gmail.com>
2016-05-10 14:49:08 -0300

    Updated AddressInfoGateway.java

--- src/main/resources/archetype-resources/src/main/java/restclient/gateway/AddressInfoGateway.java
+++ src/main/resources/archetype-resources/src/main/java/restclient/gateway/AddressInfoGateway.java
@@ -3,35 +3,47 @@
 #set( $symbol_escape = '\' )
 package ${package}.restclient.gateway;
 
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-
-import javax.xml.bind.JAXBContext;
-import javax.xml.bind.JAXBException;
-import javax.xml.bind.Unmarshaller;
-
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.log4j.Logger;
 import org.springframework.stereotype.Repository;
+
+import com.thoughtworks.xstream.XStream;
+import com.thoughtworks.xstream.io.xml.DomDriver;
 
 import ${package}.restclient.model.AddressInfo;
 
 @Repository
 public class AddressInfoGateway {
 
+	private Logger logger = Logger.getLogger(AddressInfoGateway.class);
+	
     public AddressInfo findAddressInfoByCEP(String cep) {
     	AddressInfo addressInfo = new AddressInfo();
-        JAXBContext jaxbContext;
         try {
-            jaxbContext = JAXBContext.newInstance(AddressInfoGateway.class);
-            Unmarshaller unmarshaller = jaxbContext.createUnmarshaller();
-            URL url = new URL("http://cep.desenvolvefacil.com.br/BuscarCep.php?cep=" + cep + "&ret=xml");
-            addressInfo = (AddressInfo) unmarshaller.unmarshal(url);
-        } catch (JAXBException ex) {
-            Logger.getLogger(AddressInfoGateway.class.getName()).log(Level.SEVERE, null, ex);
-        } catch (MalformedURLException ex) {
-            Logger.getLogger(AddressInfoGateway.class.getName()).log(Level.SEVERE, null, ex);
+            String url = "http://cep.desenvolvefacil.com.br/BuscarCep.php?cep=" + cep + "&ret=xml";
+            HttpClient client = new DefaultHttpClient();
+            HttpGet method = new HttpGet(url);
+            HttpResponse httpResponse = client.execute(method);
+            int statusCode = httpResponse.getStatusLine().getStatusCode();
+            if (statusCode == HttpStatus.SC_OK) {
+                InputStream inputStream = httpResponse.getEntity().getContent();
+                addressInfo = parserXMLToObject(inputStream);
+            }
+        } catch (Exception e) {
+        	logger.fatal(e);
         }
 		return addressInfo;
+    }
+    
+    private AddressInfo parserXMLToObject(InputStream inputStream) {
+        XStream xStream = new XStream(new DomDriver());
+        xStream.processAnnotations(AddressInfo.class);
+        AddressInfo addressInfo = (AddressInfo) xStream.fromXML(inputStream);
+        System.out.println(addressInfo.getCity().toUpperCase() + " - " + addressInfo.getStreet().toUpperCase());
+        return addressInfo;
     }
 }
